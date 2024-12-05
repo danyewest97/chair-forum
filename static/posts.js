a151ca9fb5c96f81103199dae635d38c3f8349a1
danyewest97 <dane20052008@gmail.com>
2024-12-05 14:37:04 -0800

    quick commit

--- static/posts.js
+++ static/posts.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
+	$(".post-body").data("is-cutoff", "false");
 	
-	$("textarea.card-text").each(function() {
-		var newText = "";
-		var oldText = $(this).text();
+	$(".post-body").each(function() {
+		var body = $(this).children("textarea.card-text");
 		
-		if (oldText.length > 10) {
-			newText = oldText.substring(0, 10);
-			newText += "..."
+		var cutoffText = "";
+		var fullText = body.text();
+		
+		if (fullText.length > 10) {
+			cutoffText = fullText.substring(0, 10);
+			cutoffText += "..."
 			
-			$(this).text(newText);
+			body.text(cutoffText);
 			$(this).data("is-cutoff", "true");
-			console.log($(this).data("is-cutoff"));
+			$(this).children("button.show-more-button").css("visibility", "visible");
+			body.data("full-text", fullText);
+			body.data("cutoff-text", cutoffText);
 		}
 		
+	});
+	
+	$("button.show-more-button").click(function() {
+		var parentDiv = $(this).parent();
+		var body = parentDiv.children("textarea.card-text");
+		
+		var fullText = body.data("full-text");
+		var cutoffText = body.data("cutoff-text");
+		
+		
+		if (parentDiv.data("is-cutoff") === "true") {
+			$(this).text("Show Less");
+			parentDiv.data("is-cutoff", "false");
+			
+			body.text(fullText);
+		} else {
+			$(this).text("Show More");
+			parentDiv.data("is-cutoff", "true");
+			
+			body.text(cutoffText);
+		}
 	});
 });
