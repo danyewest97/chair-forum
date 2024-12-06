1e0953e3d95e8b3cd9860811c151ed918c69d72e
danyewest97 <dane20052008@gmail.com>
2024-12-06 13:33:10 -0800

    (Almost) Fully fix Show More button behavior and post sizing

--- static/posts.js
+++ static/posts.js
@@ -1,26 +1,48 @@
 $(document).ready(function() {
 	$(".post-body").data("is-cutoff", "false");
 	
+	var maxCutoffCharacters = 238;
+	
+	
 	$(".post-body").each(function() {
 		var body = $(this).children("textarea.card-text");
+		
+		// Auto-resizes the textareas in the posts so that all the text in each post is shown (up to the maxCutoffCharacters character limit)
+		body.on("input", function() {
+			this.style.height = "auto";
+			this.style.height = this.scrollHeight + "px";
+		});
+		body.trigger("input");
+		
 		
 		var cutoffText = "";
 		var fullText = body.text();
 		
-		if (fullText.length > 10) {
-			cutoffText = fullText.substring(0, 10);
+		if (fullText.length > maxCutoffCharacters) {
+			cutoffText = fullText.substring(0, maxCutoffCharacters);
 			cutoffText += "..."
 			
 			body.text(cutoffText);
 			$(this).data("is-cutoff", "true");
 			$(this).children("button.show-more-button").css("visibility", "visible");
 			
+			// Auto-resizes the textareas in the posts so that all the text in each post is shown (up to the maxCutoffCharacters character limit)
+			// Doing this a second time here to update the sizes of the posts that exceeded the character limit before storing their cutoff height
+			body.on("input", function() {
+				this.style.height = "auto";
+				this.style.height = this.scrollHeight + "px";
+			});
+			body.trigger("input");
+			
 			body.data("full-text", fullText);
 			body.data("cutoff-text", cutoffText);
 			body.data("cutoff-height", body.height());
 		}
 		
+		
 	});
+	
+	
 	
 	
 	
@@ -31,7 +53,6 @@ $(document).ready(function() {
 		var fullText = body.data("full-text");
 		var cutoffText = body.data("cutoff-text");
 		var cutoffHeight = body.data("cutoff-height");
-		
 		
 		if (parentDiv.data("is-cutoff") === "true") {
 			$(this).text("Show Less");
@@ -46,8 +67,7 @@ $(document).ready(function() {
 			});
 			body.trigger("input");
 			
-			$(this).css("top", "20px");
-			parentDiv.css("margin-bottom", "30px");
+			parentDiv.css("margin-bottom", "10px");
 			
 		} else {
 			$(this).text("Show More");
@@ -57,12 +77,11 @@ $(document).ready(function() {
 			
 			
 			body.on("input", function() {
-				this.style.height = cutoffHeight;
+				body.css("height", (cutoffHeight + 4) + "px"); // Adding 4 because JQuery takes 4 pixels off height when using element.height() (idk why)
 			});
 			body.trigger("input");
 			
 			
-			$(this).css("top", "0px");
 			parentDiv.css("margin-bottom", "0px");
 		}
 	});
